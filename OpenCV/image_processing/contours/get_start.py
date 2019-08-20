1f22ed72efd60de19922fec25d8999f3b41bc464
Stark <386707933@qq.com>
2019-08-20 16:55:38 +0800

    hierarchy 解释

--- OpenCV/image_processing/contours/get_start.py
+++ OpenCV/image_processing/contours/get_start.py
@@ -7,7 +7,8 @@ import cv2 as cv
 img = cv.imread('../../resources/test.png')
 imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # 使用二进制图片（灰色图片）
 ret, thresh = cv.threshold(imgray, 127, 255, 0)  # 对图片进行阈值处理
-# 使用阈值数据找到轮廓 参数：数据源，检索模式，计算方法，返回：轮廓数组，向量关系
+# 使用阈值数据找到轮廓 参数：数据源，检索模式，计算方法，返回：轮廓数组，层级关系
+# 层级关系，表示图形数据在图片中的层级关系，对应数据解释[Next, Previous, First_Child, Parent]
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 # 画出所有的轮廓
