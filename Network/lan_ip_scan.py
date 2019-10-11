fdfd1f14f0765ace3abe880ca83b77353ec303a9
Stark <386707933@qq.com>
2019-10-11 18:08:02 +0800

    遍历局域网中主机IP

--- Network/lan_ip_scan.py
+++ Network/lan_ip_scan.py
@@ -1,41 +1,112 @@
-"""
-扫描IP
-"""
-# filename: lan_ip_scan.py
-import netifaces
-import nmap
+import os
+import re
 
 
-def get_gateways():
-    return netifaces.gateways()['default'][netifaces.AF_INET][0]
+def get_gateway_win():
+    """获取网关的IP,不完整只能获取第一个网关IP"""
+    cmd = 'ipconfig/all|find "IPv4"'
+    lines = os.popen(cmd).readlines()
+    if lines:
+        for line in lines:
+            result = re.findall(
+                r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", line)
+            if result:
+                return result[0]
 
-def get_ip_lists(gateway):
-    ip_lists = []
-    for i in range(1, 256):
-        ip_lists.append('{}{}'.format(gateway[:-1], i))
-    return ip_lists
 
-def scan_ip_survial(ip):
-    nmScan = nmap.PortScanner()
-    nmScan.scan(hosts=ip, arguments='-sP')
-    if nmScan[ip]['hostnames'][0]['name']:
-        return {'IP Address:': ip,
-                'Hostname:': nmScan[ip]['hostnames'][0]['name']
-               }
+def get_gateway_linux(net_ifac = None):
+    if net_ifac is None:
+        cmd = 'netstat -rn'
     else:
+        cmd = 'netstat -rn |grep %s'%net_ifac
+    lines = os.popen(cmd).readlines()
+    if lines:
+        for line in lines:
+            result = re.findall(
+                r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", line)
+            if result:
+                return result[0]
+
+
+def valid_ip_mac(ip):
+    ping_cmd = 'ping -n 2 {}|find "100%"'.format(ip)
+    lines = os.popen(ping_cmd).readlines()
+    if lines:
         return None
-
-def get_all_survial_hosts():
-    survial_hosts = []
-    gateway = get_gateways()
-    ip_lists = get_ip_lists(gateway)
-    for ip in ip_lists:
-        scan_rst = scan_ip_survial(ip)
-        if scan_rst:
-            survial_hosts.append(scan_rst)
-            print(scan_rst)
-    return ip_lists
+    arp_cmd = 'arp -a {}|find "{}"'.format(ip,ip)
+    lines = os.popen(arp_cmd).readlines()
+    if lines:
+        result = re.findall(
+            r'[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}', lines[0])
+        if result:
+            return str(result[0]).replace('-', '').upper()
 
 
-if __name__ == '__main__':
-    print(get_all_survial_hosts())
+def find_all_valid_ip_mac():
+    gateway = get_gateway_win()
+    if gateway is None:
+        return
+    index = str(gateway).rindex('.')
+    ip_pre = str(gateway)[0:index+1]
+    for i in range(2,255):
+        ip = ip_pre+str(i)
+        mac = valid_ip_mac(ip)
+        if mac:
+            print(ip, mac)
+
+
+def arp_a_win(gateway=None):
+    if gateway is None:
+        gateway = get_gateway_win()
+    if gateway is None:
+        return
+    index = str(gateway).rindex('.')
+    ip_pre = str(gateway)[0:index + 1]
+
+    cmd = 'arp -a'
+    lines = os.popen(cmd).readlines()
+    res = []
+    for line in lines:
+        ip = None
+        mac = None
+        result = re.findall(
+            r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", line)
+        if result:
+            ip = result[0]
+        result = re.findall(
+            r'[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}', line)
+        if result:
+            mac = str(result[0]).replace('-', '').upper()
+        if not ip is None and not mac is None and ip_pre in ip:
+            res.append((ip, mac))
+    return res
+
+
+def arp_a_linux(gateway=None):
+    if gateway is None:
+        gateway = get_gateway_linux('wlan0')
+    if gateway is None:
+        return
+    index = str(gateway).rindex('.')
+    ip_pre = str(gateway)[0:index + 1]
+
+    cmd = 'arp -a'
+    lines = os.popen(cmd).readlines()
+    res = []
+    for line in lines:
+        ip = None
+        mac = None
+        result = re.findall(
+            r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", line)
+        if result:
+            ip = result[0]
+        result = re.findall(
+            r'[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}', line)
+        if result:
+            mac = str(result[0]).replace('-', '').upper()
+        if not ip is None and not mac is None and ip_pre in ip:
+            res.append((ip, mac))
+    return res
+
+print(arp_a_win())
+# find_all_valid_ip_mac()
