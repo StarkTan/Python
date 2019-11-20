fa9ecbb326559e4648b08724067ff84fbb963115
Stark <386707933@qq.com>
2019-11-20 13:32:18 +0800

    socketio client 针对flask_socketio

--- WSClient/socketio_client.py
+++ WSClient/socketio_client.py
@@ -1,15 +1,22 @@
 import socketio
-
-sio = socketio.Client()
-start_timer = None
-
-sio.connect(url='http://localhost:5000', namespaces='/socket_io/test')
-
+import time
+import threading
 def server_response(data):
     print(data)
 
-sio.on('server_response',handler=server_response,namespace='/socket_io/test')
-sio.emit('client_event', {'data': 123}, namespace='/socket_io/test')
 
-sio.wait()
+def open_socket_io(sio):
+    sio.connect(url='http://192.168.1.1:8000', namespaces='/gsensor')
+    sio.on('server_gsensor_response', handler=server_response,namespace='/gsensor')
+    sio.emit('client_request', 'start', namespace='/gsensor')
+    sio.wait()
+    print('''x''')
 
+
+sio = socketio.Client()
+
+threading.Thread(target=open_socket_io, args=(sio,)).start()
+
+time.sleep(10)
+
+sio.eio.disconnect()  # 测试发现：需要手动关闭eio  线程结束
