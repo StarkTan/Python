7a87df46ecc2f5f87dd19cf23507750b4c51855a
Stark <386707933@qq.com>
2019-12-02 09:34:32 +0800

    网络扫描优化

--- Network/lan_ip_scan.py
+++ Network/lan_ip_scan.py
@@ -1,6 +1,7 @@
 import os
 import re
-
+import threading
+import time
 
 def get_gateway_win():
     """获取网关的IP,不完整只能获取第一个网关IP"""
@@ -107,6 +108,43 @@ def arp_a_linux(gateway=None):
         if not ip is None and not mac is None and ip_pre in ip:
             res.append((ip, mac))
     return res
+
+
+def ping_all_ip():
+    global current
+    current = 0
+
+    def get_next():
+        global current
+        current += 1
+        return current
+
+    def ping():
+        num = get_next()
+        while num < 225:
+            cmd = 'ping -n 1 192.168.10.%d' % num
+            os.popen(cmd).readlines()
+            # print(cmd)
+            num = get_next()
+    threads_live = []
+    for i in range(224):
+        thread = threading.Thread(target=ping)
+        thread.start()
+        threads_live.append(thread)
+    while threads_live:
+        temp = []
+        for thread in threads_live:
+            if thread.is_alive():
+                temp.append(thread)
+        threads_live = temp
+
+
+print(time.time())
+ping_all_ip()
+print(time.time())
+print(arp_a_win())
+print(time.time())
+
 
 # print(arp_a_win())
 # find_all_valid_ip_mac()
